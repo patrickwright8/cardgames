7c8ce27b986f368e268e096a026d5c43d5e12040
patrickwright8 <ptw804@gmail.com>
2024-08-17 21:40:03 -0400

    Implement multiprocessing

--- examples/simulate_simple_game.py
+++ examples/simulate_simple_game.py
@@ -5,6 +5,7 @@ for a simple card game.
 
 ### Imports ###
 import numpy as np
+from multiprocessing import Process, Lock
 from stable_baselines3 import DQN
 
 from cardgames import games
@@ -15,16 +16,22 @@ n_runs = int(1e4 / n_decks)
 total_timesteps = int(1e5)
 
 ### Functions ###
-def simulate_run(name, action_func):
+def simulate_run(l, name, action_func):
     env = games.GameSimulator(n_decks, games.simple_rank_scores)
-    print(f"Using {name} method:")
-    
+
     rewards = np.zeros((n_runs,))
     for i in range(n_runs):
         rewards[i] = sum(env.simulate_run(action_func))
 
-    print(f"Mean reward per episode after {n_runs} runs:" 
-          f" {rewards.mean() :.2f}\n")
+    l.acquire()
+    try:
+        print(
+            f"Using {name} method: \n"
+            f"Mean reward per episode after {n_runs} runs:" 
+            f" {rewards.mean() :.2f}\n"
+        )
+    finally:
+        l.release()
 
 def train_rl():
     env = games.GameBase(n_decks, games.simple_rank_scores)
@@ -34,14 +41,16 @@ def train_rl():
 
 def main():
     env = games.GameSimulator(n_decks, games.simple_rank_scores)
+    env.reset()
     stat_methods = {
         "random"        : env.random,
         "mean score"    : env.mean_observation,
         "expected score": env.expected_observation, 
     }
 
+    lock = Lock()
     for name, func in stat_methods.items():
-        simulate_run(name, func)
+        Process(target=simulate_run, args=(lock, name, func)).start()
 
     train_rl()
         
