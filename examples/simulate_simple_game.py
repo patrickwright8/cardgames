f5e5ec383ce3799be940ed2b93b7ed0fbdfcb07e
patrickwright8 <ptw804@gmail.com>
2024-08-17 21:19:03 -0400

    Update to match games.py api

--- examples/simulate_simple_game.py
+++ examples/simulate_simple_game.py
@@ -5,45 +5,45 @@ for a simple card game.
 
 ### Imports ###
 import numpy as np
-import json
-from stable_baselines3 import DQN, PPO, A2C
+from stable_baselines3 import DQN
 
 from cardgames import games
 
 ### CONSTANTS ###
 n_decks = 1
-n_runs = int(1e5)
+n_runs = int(1e4 / n_decks)
+total_timesteps = int(1e5)
 
 ### Functions ###
-def simulate_run(env : games.GameSimulator, name, action_func):
+def simulate_run(name, action_func):
+    env = games.GameSimulator(n_decks, games.simple_rank_scores)
     print(f"Using {name} method:")
     
     rewards = np.zeros((n_runs,))
     for i in range(n_runs):
         rewards[i] = sum(env.simulate_run(action_func))
 
-    print(f"Mean reward after {n_runs} runs: {rewards.mean() :.2f}\n")
+    print(f"Mean reward per episode after {n_runs} runs:" 
+          f" {rewards.mean() :.2f}\n")
 
-def train_rl(env):
+def train_rl():
     env = games.GameBase(n_decks, games.simple_rank_scores)
-    agents = [A2C] #, DQN, PPO]
-    for agent in agents:
-        model = agent("MlpPolicy", env, verbose=1, seed=0)
-        model.learn(total_timesteps=n_runs / 10, log_interval=n_runs / 100)
+
+    model = DQN("MlpPolicy", env, verbose=1, seed=0, exploration_final_eps=0)
+    model.learn(total_timesteps=total_timesteps)
 
 def main():
     env = games.GameSimulator(n_decks, games.simple_rank_scores)
     stat_methods = {
         "random"        : env.random,
-        "mean score"    : env.mean_score,
-        "expected score": env.expected_score, 
+        "mean score"    : env.mean_observation,
+        "expected score": env.expected_observation, 
     }
 
     for name, func in stat_methods.items():
-        simulate_run(env, name, func)
+        simulate_run(name, func)
 
-    env = games.GameSimulator(n_decks, games.simple_rank_scores)
-    train_rl(env)
+    train_rl()
         
 if __name__ == "__main__":
     main()
