26c1126e65e9216cf39e98013da286869dac5f0b
patrickwright8 <ptw804@gmail.com>
2024-08-19 21:40:26 -0400

    fix: improve versioning with pyproject.toml

--- cardgames/__init__.py
+++ cardgames/__init__.py
@@ -1,4 +1,9 @@
 """
 A Python package for card games.
 """
-__version__ = "0.1.1"
+
+from pathlib import Path as _Path
+import toml as _toml
+
+_data = _toml.load(_Path(__file__).parent.parent / "pyproject.toml")
+__version__ = _data["project"]["version"]
